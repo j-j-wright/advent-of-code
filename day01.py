3236703c707eafe2cd54fda236ee6ee014f5bc50
Josh Wright <jjwright4472@gmail.com>
2023-07-05 21:47:17 -0400

    day 01 solution

---
+++ day01.py
@@ -0,0 +1,16 @@
+with open('inputs\day01_input.txt') as f:
+    # part one
+    inpt = f.read()
+    floor = 0    
+    for i in range(len(inpt)):
+        floor += 1 if inpt[i] == '(' else -1
+    print('part one: ' + str(floor))
+
+    # part two
+    floor = 0    
+    for i in range(len(inpt)):
+        if floor < 0:
+            print('part two: ' + str(i))
+            break
+        floor += 1 if inpt[i] == '(' else -1
+
